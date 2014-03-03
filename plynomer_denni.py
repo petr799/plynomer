618d951b999081345bef04d6d02034c2de3e2de3
petr799 <ja@petrholcman.cz>
2014-03-03 11:05:04 +0100

    zmena vyberu hodnot ze vcerejska z plynomer_hodinova

--- plynomer_denni.py
+++ plynomer_denni.py
@@ -9,12 +9,12 @@ try:
     cursor = con.cursor()
 
 # secteme hodnoty ze sloupce spotreba za vcerejsi den
-    cursor.execute("SELECT SUM(spotreba) FROM `plynomer_hodinova` WHERE time >= (CURDATE() - 1) AND time < CURDATE()")
+    vcerejsi_datum = date.today() - timedelta(1)
+    cursor.execute("SELECT SUM(spotreba) FROM `plynomer_hodinova` WHERE time >= %s - 1) AND time < CURDATE()",(vcerejsi_datum))
     spotreba_den = cursor.fetchone()
     cursor.execute("SELECT SUM(spotreba) FROM plynomer_hodinova")
     spotreba_celkem = cursor.fetchone()
-    vcerejsi_datum = date.today() - timedelta(1)
-    cursor.execute("""INSERT INTO plynomer_denni (den, spotreba_den, spotreba_celkem) VALUES (%s, %s, %s)""",(vcerejsi_datum, spotreba_den[0], spotreba_celkem[0]))
+    cursor.execute("""INSERT INTO plynomer_denni den, spotreba_den, spotreba_celkem) VALUES (%s, %s, %s)""",(vcerejsi_datum, spotreba_den[0], spotreba_celkem[0]))
     con.commit()
     con.close()
 except:
